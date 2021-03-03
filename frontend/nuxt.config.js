b208b121f27868b1bd62874bad1bd6dbfa094aec
Victor <fisheve@mx.lakeforest.edu>
2021-03-02 19:20:41 -0600

    Removed Axios

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -30,16 +30,11 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
   ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
